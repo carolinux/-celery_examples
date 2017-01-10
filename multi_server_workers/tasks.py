f9b86fa9a0598cca116d3406c5b48379595052d0
carolinux <carolinegr@gmail.com>
2017-01-10 15:08:08 +0200

    Some modifications to not have synchronous subtasks

--- multi_server_workers/tasks.py
+++ multi_server_workers/tasks.py
@@ -1,6 +1,8 @@
 import time
+from functools import partial
 
 from celery import chord
+from celery.result import allow_join_result
 
 from . import celeryapp
 
@@ -19,14 +21,17 @@ def slowadd(x,y):
     return x+y
 
 @celeryapp.task
-def tsum(list_of_num):
-    return sum(list_of_num)
+def tsum(list_of_num, x=None, y=None):
+    return sum(list_of_num)+x+y
 
 @celeryapp.task
 def run_chord(x,y):
-    callback = tsum.subtask()
-    header = [add.subtask((i, i)) for i in xrange(100)]
+    callback = tsum.subtask(x=x,y=y)
+    header = [add.subtask((i, i)) for i in xrange(3)]
     result = chord(header)(callback)
-    res = result.get()
-    print "Chord result: {}".format(res)
+    # tasks waiting for other tasks to finish in this way is inefficient: 
+    # http://docs.celeryq.org/en/latest/userguide/tasks.html#task-synchronous-subtasks
+    # with allow_join_result():
+    #     res = result.get()
+    #     print "Chord result: {}".format(res)
     return res
--- send_task.py
+++ send_task.py
@@ -1,8 +1,11 @@
 from multi_server_workers import celeryapp
 
+import time
 import sys
 
 task_name = sys.argv[1]
 task = celeryapp.send_task("multi_server_workers.tasks.{}".format(task_name),
                          kwargs={"x":12, "y":40})
 
+res = task.get() # synchronous wait
+print "Result {}".format(res)
