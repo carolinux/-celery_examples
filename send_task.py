efe2803fbdec177c66fa88ff037425abb32c4992
root <root@ubuntu-512mb-fra1-01.localdomain>
2017-01-10 13:50:21 +0000

    misc fixes

--- send_task.py
+++ send_task.py
@@ -3,10 +3,15 @@ from multi_server_workers import celeryapp
 import time
 import sys
 
+from celery.result import AsyncResult
+
 task_name = sys.argv[1]
 task = celeryapp.send_task("multi_server_workers.tasks.{}".format(task_name),
                          kwargs={"x":12, "y":40})
 
-res = task.get().get() # synchronous wait
+res = task
+
+while(isinstance(res,AsyncResult)):
+    res = res.get() # synchronous wait
         
 print res
